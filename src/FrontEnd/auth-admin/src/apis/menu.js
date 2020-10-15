7c09a1f09b88363aa37939f6f5b876e8951ab3cc
JackWang <wangchangjian1130@gmail.com>
2020-10-15 23:53:41 +0800

    no message

---
+++ src/FrontEnd/auth-admin/src/apis/menu.js
@@ -0,0 +1,53 @@
+const Menu = [{
+        title: "基础数据",
+        icon: "mdi-cog-outline",
+        items: [{
+                name: "organization",
+                title: "组织信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "user",
+                title: "用户信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "position",
+                title: "岗位信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "role",
+                title: "角色信息",
+                icon: "mdi-folder"
+            },
+        ]
+    },
+    {
+        title: "系统设置",
+        icon: "mdi-cog-outline",
+        items: [{
+                name: "module",
+                title: "模块信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "api",
+                title: "接口信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "permission",
+                title: "权限信息",
+                icon: "mdi-folder"
+            },
+            {
+                name: "setting",
+                title: "数据字典",
+                icon: "mdi-folder"
+            }
+        ]
+    }
+];
+
+export default Menu;
---
+++ src/FrontEnd/auth-admin/src/components/layouts/Layout.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-card class="mx-auto drawer" width="256" tile>
+      <v-navigation-drawer permanent>
+        <v-list class="menu-list">
+          <v-list-item link>
+            <v-list-item-avatar>
+              <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="title"> John Leider </v-list-item-title>
+              <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon>mdi-menu-down</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list dense expand>
+          <v-list-group v-for="(menu, m) in menus" :key="m">
+            <template v-slot:activator>
+              <v-list-item-icon>
+                <v-icon v-text="menu.icon" small></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ menu.title }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="(item, i) in menu.items" :key="i">
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import menu from "../../apis/menu.js";
+
+export default Vue.extend({
+  name: "Layout",
+
+  data: () => ({
+    item: 0,
+    menus: menu,
+  }),
+});
+</script>
+
+<style lang="less" scoped>
+.drawer {
+  position: absolute;
+  height: 100vh;
+  top: -64px;
+}
+
+.drawer .menu-list {
+  margin-top: 64px;
+}
+</style>
