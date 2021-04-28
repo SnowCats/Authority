0bcdb4575196d09bd51acb09c0780fab7de62f23
Jack <wangchangjian1130@gmail.com>
2021-04-28 17:11:59 +0800

    添加IdentityServer4配置文件

---
+++ src/Backend/Core/Auth.Core/IdentityServer4/Ids4Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Core.IdentityServer4
+{
+    /// <summary>
+    /// IdentityServer4 Configuration
+    /// </summary>
+    public static class Ids4Config
+    {
+        /// <summary>
+        /// Add Config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddIdentityServer(this IServiceCollection services)
+        {
+
+        }
+    }
+}
